87f5e3f72385a08f4c3ae851f82729d58f7cbec6
Meghna Rao <mrao123@terpmail.umd.edu>
2023-03-12 16:21:23 -0400

    newList

--- client/lab_5/script.js
+++ client/lab_5/script.js
@@ -69,7 +69,7 @@ async function mainEvent() { // the async keyword means we can make API requests
 
     console.log(formProps);
     const newList = filterList(currentList, formProps.resto);
-    console.log(newlist);
+    console.log(newList);
   });
 
   /*
