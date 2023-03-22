d8c39007f7eb09a50ac1df725c812d626bde0d9d
Meghna Rao <mrao123@terpmail.umd.edu>
2023-03-22 18:55:37 -0400

    js

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -12,7 +12,7 @@ function injectHTML(list) {
   console.log('fired injectHTML')
   const target = document.querySelector('#restaurant_list')
   target.innerHTML = '';
-  list.forEach((item, index) => {
+  list.forEach((item) => {
     const str = `<li>${item.name}</li>`;
     target.innerHTML += str
   })
@@ -89,6 +89,7 @@ async function mainEvent() { // the async keyword means we can make API requests
   generateListButton.addEventListener('click', (event) => {
       console.log('generate new list');
       const restaurantsList = cutRestaurantList(currentList);
+      console.log(restaurantsList);
       injectHTML(restaurantsList);
       
   })
